7c637c85cc6b4b60189864f401927bd6ccde2e0e
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2019-03-16 14:38:32 +0100

    Add Libraries and Node

---
+++ InverseKinematicsResearch/InverseKinematicsResearch/Node.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Particle.h"
+
+struct Connection
+{
+
+};
+
+class Node
+{
+
+};
+
+class BaseNode
+{
+
+};
