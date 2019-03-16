6ad24421588fa68df4cd5d07dcf8a4ffd96a614e
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2019-03-16 19:30:32 +0100

    Add links between joints

--- InverseKinematicsResearch/InverseKinematicsResearch/Node.h
+++ InverseKinematicsResearch/InverseKinematicsResearch/Node.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
-#define GIZMO_SCALE_MATRIX glm::scale(glm::mat4(1.0f), glm::vec3(0.2f))
+#define GIZMO_SIZE 0.2f
+#define GIZMO_SCALE_MATRIX glm::scale(glm::mat4(1.0f), glm::vec3(GIZMO_SIZE))
 
 struct Connection;
 struct Node;
@@ -48,7 +49,7 @@ public:
 		DrawCurrent(shader, VAO);
 		for (int i = 0; i < link.children.size(); i++)
 		{
-			//add link drawing
+			DrawLink(shader, VAO, link.children[i]);
 			link.children[i]->Draw(shader, VAO);
 		}
 	}
@@ -61,7 +62,7 @@ public:
 		}
 		else
 		{
-			return link.parent->GetModelMatrix() * glm::translate(glm::mat4_cast(rotation), glm::vec3(link.length, 0.0f, 0.0f));
+			return link.parent->GetModelMatrix() * glm::translate(glm::mat4(1.0f), glm::vec3(link.length, 0.0f, 0.0f)) * glm::mat4_cast(rotation);
 		}
 	}
 
@@ -74,6 +75,20 @@ protected:
 		glBindVertexArray(VAO);
 		glLineWidth(2.0f);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
+	void DrawLink(Shader shader, unsigned int VAO, Node* child)
+	{
+		glm::mat4 model = GetModelMatrix() *
+						  glm::translate(glm::mat4(1.0f), glm::vec3(child->link.length * 0.5f, 0.0f, 0.0f)) *
+						  glm::scale(glm::mat4(1.0f), glm::vec3(child->link.length, GIZMO_SIZE * 0.25f, GIZMO_SIZE * 0.25f));
+
+		shader.use();
+		shader.setVec3("color", 1.0f, 0.5f, 0.2f);
+		shader.setMat4("model", model);
+		glBindVertexArray(VAO);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 	}
 };
