588c1d8e712c20f20cfc11d076bae352843dfc03
Eryk Dobrosz <eryk.dobrosz@gmail.com>
2019-03-25 23:23:37 +0100

    Add quaternion device functions headers.

--- InverseKinematicsResearch/InverseKinematicsResearch/quaternion_operations.cuh
+++ InverseKinematicsResearch/InverseKinematicsResearch/quaternion_operations.cuh
@@ -1,3 +1,15 @@
 #pragma once
 #include "cuda_runtime.h"
 #include <math.h>
+
+__device__ float4 eulerToQuaternion(float3 eulerAngles)
+{
+	float4 ret;
+	return ret;
+}
+
+__device__ float3 quaternionToEuler(float4 quaternion)
+{
+	float3 ret;
+	return ret;
+}
